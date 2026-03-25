347a3c6384990c4cb2379e613d415338c10ddecc
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-03-25 20:41:12 +0530

    add: in-memory store implementation for B+Tree nodes in storage package

---
+++ storage/io.go
@@ -0,0 +1,52 @@
+package storage
+
+import "fmt"
+
+type store interface {
+	// append appends a node at the end of the store.
+	append(node *btreeNode) error
+	// update updates the node at the provided node's offset.
+	update(node *btreeNode) error
+	// fetch returns the node at the offset in the store.
+	fetch(offset uint64) (*btreeNode, error)
+	nextLSN() uint64
+	incrLSN()
+	// fsync persists the data in stable storage (on disk).
+	fsync() error
+}
+
+type memoryStore struct {
+	pages   []*btreeNode
+	lastKey uint32
+}
+
+func (ms *memoryStore) nextLSN() uint64 {
+	return 0
+}
+
+func (ms *memoryStore) incrLSN() {
+	return
+}
+
+func (ms *memoryStore) fsync() error {
+	return nil
+}
+
+func (ms *memoryStore) append(node *btreeNode) error {
+	node.position = uint64(len(ms.pages))
+	ms.pages = append(ms.pages, node)
+	return nil
+}
+
+func (ms *memoryStore) update(node *btreeNode) error {
+	index := node.position
+	ms.pages[index] = node
+	return nil
+}
+
+func (ms *memoryStore) fetch(offset uint64) (*btreeNode, error) {
+	if offset >= uint64(len(ms.pages)) {
+		return nil, fmt.Errorf("page does not exist")
+	}
+	return ms.pages[offset], nil
+}
